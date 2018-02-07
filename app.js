5460db46a0544105e347cfb4e0f3ef37e536fc93
Priyajathar <priyajathar@india.com>
2018-02-07 09:41:28 +0530

    update

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
+  app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
